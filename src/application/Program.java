5d50dcd277f9cfcaae5928999eea2e8f4020ab9f
Alex Pierini <alex_pierini@hotmail.com>
2020-03-23 15:02:21 -0400

    Classe d'entité de département.

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departement;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departement obj = new Departement(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
